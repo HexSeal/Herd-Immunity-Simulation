13d82f282585ea787698f839de20ac816aab0248
HexSeal <maxfin13@gmail.com>
2019-10-09 19:35:10 -0700

    [add] create population done. Working on run and timestep

--- logger.py
+++ logger.py
@@ -1,3 +1,5 @@
+import os
+
 class Logger(object):
     ''' Utility class responsible for logging all interactions during the simulation. '''
     # TODO: Write a test suite for this class to make sure each method is working
@@ -12,7 +14,7 @@ class Logger(object):
 
     def write_metadata(self, pop_size, vacc_percentage, virus_name, mortality_rate,
                        basic_repro_num):
-        metadata = ("Population size: {}\t", "Vacc Percentage {}\t", "Virus Name {}\t", "Mortality Rate: {}\t", "Basic Reproduction Number: {}\n".format(pop_size, vacc_percentage, virus_name, mortality_rate, basic_repro_num))
+        metadata = ("Population size: {}\t Vacc Percentage {}\t Virus Name {}\t Mortality Rate: {}\t Basic Reproduction Number: {}\n".format(pop_size, vacc_percentage, virus_name, mortality_rate, basic_repro_num))
         f = open(self.file_name, "w+")
         f.write(metadata)
 
@@ -45,24 +47,16 @@ class Logger(object):
             f.write(status)
 
     def log_time_step(self, time_step_number):
-        ''' STRETCH CHALLENGE DETAILS:
-
-        If you choose to extend this method, the format of the summary statistics logged
-        are up to you.
-
-        At minimum, it should contain:
-            The number of people that were infected during this specific time step.
-            The number of people that died on this specific time step.
-            The total number of people infected in the population, including the newly infected
-            The total number of dead, including those that died during this time step.
-
-        The format of this log should be:
-            "Time step {time_step_number} ended, beginning {time_step_number + 1}\n"
-        '''
-        # TODO: Finish this method. This method should log when a time step ends, and a
-        # new one begins.
-        # NOTE: Here is an opportunity for a stretch challenge!
-        pass
+        time_step_summary = (f"""\
+            {'=+'*25}\nTime step {time_step_number} ended\nInfected this time step: {current_infected}\n
+            Died this time step: {dead_this_step}\nTotal Population that has been infected = {total_infected}\n
+            Total Deaths = {total_dead}\n{'-'*50}\n""")
+        #Removes indentation from the string, and reformats it to allign with text file.
+        self.text_formatting(self.file_name, time_step_summary)
+        #If it's the last step, function will not write to file
+        if current_infected != 0:
+            with open(self.file_name, "a") as logs_f:
+                logs_f.write(f"Beginning step {time_step_number + 1}\n\n")
 
     def test_logger(self):
         assert self.file_name is "logs.txt"
---
+++ logger_test.py
@@ -0,0 +1,11 @@
+from logger import Logger
+from person import Person
+from virus import Virus
+from simulation import Simulation
+import pytest
+import io
+import sys
+import os.path
+    
+def test_logger(self):
+    assert self.file_name is "logs.txt"
